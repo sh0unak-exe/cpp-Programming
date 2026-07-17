83a9d1cb4ae6b21c521a4f9bb6193c578cdc7eaa
Sh0unak <shounakdesai4@gmail.com>
2026-07-17 10:55:14 +0530

    Add files via upload

---
+++ Maths.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=3;
+int b=8;
+cout<<"Addition:"<<a+b<<endl;
+cout<<"Substract:"<<a-b<<endl;
+cout<<"Multiply:"<<a*b<<endl;
+cout<<"Divsion:"<<a/b<<endl;
+cout<<"Modulus:"<<a%b<<endl;
+return 0;
+}
+
+
+
