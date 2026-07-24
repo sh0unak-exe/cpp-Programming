4e386167ebdb8d11b25450dcdf30dbd743312d8e
Sh0unak <shounakdesai4@gmail.com>
2026-07-24 10:48:49 +0530

    Add files via upload

---
+++ rect.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int 1,b;
+cin >> 1 << b;
+cout << "Area = " << 1*b<< endl;
+cout << "Perimeter = " << 2* (1 + b);
+return 0;
+}
---
+++ swap2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cin >> a >> b;
+a = a + b;
+b = a - b;
+c = a - b;
+cout << a << " " << b;
+return 0;
+}
